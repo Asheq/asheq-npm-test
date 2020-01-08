e5c9047603d3d12ee169431b1844a0a489c4156f
Asheq Imran <Asheq89@gmail.com>
2020-01-08 15:25:35 -0800

    Update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const lodash = require('lodash');
-const asheqNpmTestSub = require('asheq-npm-test-sub');
+const asheqNpmTestSubLodash = require('asheq-npm-test-sub');
 
 console.log('lodash version in asheq-npm-test:', lodash.VERSION);
+console.log('asheqNpmTestSubLodash version in asheq-npm-test:', asheqNpmTestSubLodash.VERSION);
